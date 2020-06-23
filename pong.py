c760965917cd3c399d988e94ab7bb8ec6d7a090e
mistyhippo <Valentinabrent@gmail.com>
2020-06-22 18:59:34 -0700

    create paddle A, paddle B, and Ball.  put main game loop towards the bottom, remove anaconda

--- pong.py
+++ pong.py
@@ -1,1 +1,43 @@
 # Simple Pong game in Python3
+#By @mistyhippo
+
+import turtle
+#import tkinter as tk
+
+# window settings
+win = turtle.Screen()
+win.title("Pong by @mistyhippo")
+win.bgcolor("black")
+win.setup(width=800, height=600)
+# stops the window from autoupdating
+win.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
+# main game loop
+while True:
+    win.update()
